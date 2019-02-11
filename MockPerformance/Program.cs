fad330740fcc01bd2a4448640c3070e78c5c0eaa
Grigory Bushuev <grigorybushuev@hotmail.com>
2019-02-11 09:10:04 -0500

    Added mock frameworks

--- MockPerformance/Program.cs
+++ MockPerformance/Program.cs
@@ -21,10 +21,9 @@ namespace MockPerformance
             var sb = new StringBuilder();
             for (var i = 0; i < 3000; i++)
             {
-                sb.AppendLine($"{Environment.NewLine}            public Object Propery{i} {{ get; set; }}");
-                sb.AppendLine($"{Environment.NewLine}            public void Method{i} (Object obj) {{ }}");
+                sb.AppendLine($"\r\n            public Object Propery{i} {{ get; set; }}");
+                sb.AppendLine($"\r\n            public void Method{i} (Object obj) {{ }}");
             }
-
             var code = String.Format(ClassTemplate, sb);
             System.IO.File.WriteAllText(@"C:\sources\MockPerformance\MockPerformance\BloatedClass.cs", code);
         }
